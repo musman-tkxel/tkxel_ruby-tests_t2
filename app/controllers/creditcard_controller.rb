f219049ab254fff08d69ed933bced8a4a213eb7d
Muhammad Usman <muhammad.usman@tkxel.com>
2013-09-30 09:49:05 +0500

    Functions Logic Updated

--- app/controllers/creditcard_controller.rb
+++ app/controllers/creditcard_controller.rb
@@ -14,7 +14,11 @@ class CreditcardController < ApplicationController
       @newCardNumber = false
     elsif (@execute == "APPEND CHECK SUM")
       @temp_result = find_check_sum? @card_num
-      @newCardNumber = @card_num.to_s + @temp_result.to_s
+      if(@temp_result.to_s.eql? "In-Valid Combination")
+        @newCardNumber = "In-Valid Combination"
+      else
+        @newCardNumber = @card_num.to_s + @temp_result.to_s
+      end
       @isValid = false
     end
 
@@ -31,6 +35,12 @@ class CreditcardController < ApplicationController
 
 
   def credit_card_valid?(account_number)
+  begin
+
+    if account_number.to_s.length == 0
+      return
+    end
+
     digits = account_number.scan(/./).map(&:to_i)
     check = digits.pop
 
@@ -39,16 +49,25 @@ class CreditcardController < ApplicationController
     end.flatten.inject(:+)
 
     (10 - sum % 10) == check
+  rescue => e
+    "In-Valid Combination"
+  end
   end
 
   def find_check_sum?(account_number)
+  begin
     digits = account_number.scan(/./).map(&:to_i)
 
     sum = digits.reverse.each_slice(2).map do |x, y|
       [(x * 2).divmod(10), y]
     end.flatten.inject(:+)
 
-    (10 - sum % 10)
+
+      (10 - sum % 10)
+  rescue => e
+      "In-Valid Combination"
+  end
+
   end
 
 end
